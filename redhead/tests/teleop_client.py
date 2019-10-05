2b5bb93e8e2e06985fc044b2126fe23a35b016f0
Scott Umsted <scottumsted@gmail.com>
2019-10-05 09:04:29 -0500

    teleop client created

--- redhead/tests/teleop_client.py
+++ redhead/tests/teleop_client.py
@@ -4,6 +4,18 @@ class TeleopClient():
     UDP_ADDRESS = '192.168.2.58'
     UDP_PORT = 8484
     MESSAGE = "%04d%04d"
+    THROTTLE = .75
+    KEY_MAP = {
+        'q': (50,100),
+        'w': (100,100),
+        'e': (100,50),
+        'a': (-100,100),
+        's': (0,0),
+        'd': (100,-100),
+        'z': (-50,-100),
+        'x': (-100,-100),
+        'c': (-100,-50)
+    }
 
     def __init__(self):
         print("init")
@@ -15,7 +27,14 @@ class TeleopClient():
 
     def start(self):
         print("start")
-        self.send(50,0)
+        k = None
+        while True:
+            k = input("stop or key...\nqwe\nasd\nzxc\n? ")
+            if k == "stop":
+                self.send(0, 0)
+                break
+            self.send(TeleopClient.KEY_MAP[k[0]][0]*TeleopClient.THROTTLE, TeleopClient.KEY_MAP[k[0]][1]*TeleopClient.THROTTLE)
+        print("stop")
 
 if __name__=='__main__':
     tc = TeleopClient()

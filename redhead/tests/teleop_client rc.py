645519b544a0be988713569ab41b7d7c9efa717e
Scott Umsted <scottumsted@gmail.com>
2020-05-14 08:37:08 -0500

    new rc teleop client

---
+++ redhead/tests/teleop_client rc.py
@@ -0,0 +1,41 @@
+import socket
+
+class TeleopClient():
+    UDP_ADDRESS = '192.168.2.58'#'10.42.0.1'#'192.168.2.58'
+    UDP_PORT = 8484
+    MESSAGE = "%04d%04d%04d"
+    THROTTLE = .75
+    KEY_MAP = {
+        'q': (1,150,1),
+        'w': (0,150,1),
+        'e': (2,150,1),
+        # 'a': (-100,100),
+        's': (0,0,0),
+        # 'd': (100,-100),
+        'z': (1,150,2),
+        'x': (0,150,2),
+        'c': (2,150,2)
+    }
+
+    def __init__(self):
+        print("init")
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        
+    def send(self, tiller, direction, velocity):
+        print("send")
+        self.sock.sendto(str.encode(TeleopClient.MESSAGE%(tiller,direction,velocity)), (TeleopClient.UDP_ADDRESS, TeleopClient.UDP_PORT))
+
+    def start(self):
+        print("start")
+        k = None
+        while True:
+            k = input("stop or key...\nqwe\nasd\nzxc\n? ")
+            if k == "stop":
+                self.send(0, 0)
+                break
+            self.send(TeleopClient.KEY_MAP[k[0]][0], TeleopClient.KEY_MAP[k[0]][1], TeleopClient.KEY_MAP[k[0]][2])
+        print("stop")
+
+if __name__=='__main__':
+    tc = TeleopClient()
+    tc.start()

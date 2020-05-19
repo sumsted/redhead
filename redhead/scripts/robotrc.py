178a5e0a485806d1942530bc82088ddfa20b1b90
Scott Umsted <scott@wildidea.xyz>
2020-05-19 08:56:20 -0500

    testing new delay and step on speed ramp

--- redhead/scripts/robotrc.py
+++ redhead/scripts/robotrc.py
@@ -16,8 +16,8 @@ class Robot(object):
     FORWARD = Adafruit_MotorHAT.FORWARD
     STOP = Adafruit_MotorHAT.RELEASE
     BACKWARD = Adafruit_MotorHAT.BACKWARD
-    SPEED_STEP = 50
-    DELAY = .01
+    SPEED_STEP = 75
+    DELAY = 0
 
     def __init__(self, addr=0x60, tiller_id=1, drive_id=2, tiller_trim=0, drive_trim=0,
                  stop_at_exit=True):
